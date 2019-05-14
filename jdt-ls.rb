8d2344ec0561b31c4f4bd454cf43d713031304d2
Eduard Ganiukov <eduard.ganiukov@swisscom.com>
2019-05-14 13:20:11 +0200

    jdt-ls: 0.38.0-201905021936
    Signed-off-by: Eduard Ganiukov <eduard.ganiukov@swisscom.com>

--- jdt-ls.rb
+++ jdt-ls.rb
@@ -2,9 +2,9 @@ class JdtLs < Formula
   desc "A Java language specific implementation of the Language Server Protocol"
   homepage "https://github.com/eclipse/eclipse.jdt.ls"
 
-  url "http://download.eclipse.org/jdtls/snapshots/jdt-language-server-0.37.0-201904232207.tar.gz"
-  sha256 "196dbf954f6fc5d3614d77f181855e99ea46277bbd57d772c51157055ab8c76d"
-  version "0.37.0-201904232207"
+  url "http://download.eclipse.org/jdtls/milestones/0.38.0/jdt-language-server-0.38.0-201905021936.tar.gz"
+  sha256 "83f1fe763992b35f54974159d70f5849d5947d123e821d71c52c2c9baee28f61"
+  version "0.38.0-201905021936"
 
   bottle :unneeded
 
@@ -24,15 +24,16 @@ class JdtLs < Formula
         -Declipse.application=org.eclipse.jdt.ls.core.id1 \
         -Dosgi.bundles.defaultStartLevel=4 \
         -Declipse.product=org.eclipse.jdt.ls.core.product \
+        -Dfile.encoding=utf8 \
         -Dlog.protocol=true \
         -Dlog.level=ALL \
         -noverify \
         $JDT_LS_HEAP_SIZE \
         -jar "$JDT_LS_LAUNCHER" \
-        -configuration "$JDT_LS_HOME/config_mac" \
         --add-modules=ALL-SYSTEM \
         --add-opens java.base/java.util=ALL-UNNAMED \
         --add-opens java.base/java.lang=ALL-UNNAMED \
+        -configuration "$JDT_LS_HOME/config_mac" \
         "$@"
     EOS
   end
